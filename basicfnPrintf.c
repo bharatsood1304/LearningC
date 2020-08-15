753efc39a58c4c7a4f51194f5dae52f90093ed0d
Bharat Sood <bhartsood0401@gmail.com>
2020-08-14 21:24:21 -0400

    Bharat Sood

---
+++ basicfnPrintf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int two = 2, three = 3, six = 6;
+    int result = (two + three) * six/three;
+    printf("abc: (%d + %d) * %d / %d",two,three,six,three);
+    printf("result is: %d", result);
+}
---
+++ unsignedshortInt.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<limits.h>
+
+int main(){
+    unsigned short int var1 = 0;
+    unsigned short int var2 = USHRT_MAX;
+    printf("the range of short unsigned integer: %u to %u", var1, var2);
+    return 0;
+}
